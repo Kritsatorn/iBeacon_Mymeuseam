cc7a0955b2a36d35e24df917c2882e46e5cfbdc1
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2020-09-20 12:23:08 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { IBeacon } from '@ionic-native/ibeacon/ngx';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    IBeacon,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
